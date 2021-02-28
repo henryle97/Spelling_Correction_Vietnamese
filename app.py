cd29dc882bda63d6690b8988caec158d3ddd2cdf
Hisiter <leeyueshing19@gmail.com>
2021-02-28 22:44:20 +0700

    add gen noise

--- app.py
+++ app.py
@@ -3,21 +3,32 @@ import streamlit as st
 import SessionState
 import numpy as np
 from predictor import Predictor
+from dataset.add_noise import SynthesizeData
 
-state = SessionState.get(text_correct="")
+state = SessionState.get(text_correct="", input="")
 
 
 def main():
-    model = load_model()
+    model, synther = load_model()
     st.title("Chương trình sửa lỗi chính tả tiếng việt")
     # Load model
 
-    text_input = st.text_area("Gõ câu sai tại đây:")
+    text_input = st.text_area("Gõ câu sai tại đây:", value=state.input)
+    if st.button("Sinh lỗi"):
+        noise_text = synther.add_noise(text_input, percent_err=0.15)
+        state.output = noise_text
+        text_input = st.text_area("Câu sai sinh:", value=state.output)
     if st.button("Correct"):
+        text_input = model.spelling_correct(text_input)
+
         state.text_correct = model.spelling_correct(text_input)
 
+
+    st.text("Câu nhiễu: ")
+    st.success(text_input)
     st.text("Kết quả:")
     st.success(state.text_correct)
+
     # state.sync()
 
 
@@ -25,7 +36,8 @@ def main():
 def load_model():
     print("Loading model ...")
     model = Predictor(weight_path='weights/seq2seq.pth')
-    return model
+    synther = SynthesizeData()
+    return model, synther
 
 
 if __name__ == "__main__":
